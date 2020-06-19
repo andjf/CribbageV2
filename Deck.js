ea4c8788388c5ab5a64476ceb9522aeeb58cb440
AndJ <andjferrin@gmail.com>
2020-06-19 10:25:39 -0700

    Formatting Code
    Formatting Code using JSBeautifier (https://beautifier.io/)

--- Deck.js
+++ Deck.js
@@ -1,75 +1,75 @@
 class Deck {
 
-  constructor(shouldStartFull) {
-    this.cards = new Array(0);
-    if (shouldStartFull) {
-      let suits = ["S", "H", "C", "D"];
-      let extra = ["J", "Q", "K"];
-      for (var suitIndex = 0; suitIndex < suits.length; suitIndex++) {
-        this.cards.push(new Card("A" + suits[suitIndex]));
-        for (let value = 2; value <= 10; value++) {
-          this.cards.push(new Card(value.toString() + suits[suitIndex]));
+    constructor(shouldStartFull) {
+        this.cards = new Array(0);
+        if (shouldStartFull) {
+            let suits = ["S", "H", "C", "D"];
+            let extra = ["J", "Q", "K"];
+            for (var suitIndex = 0; suitIndex < suits.length; suitIndex++) {
+                this.cards.push(new Card("A" + suits[suitIndex]));
+                for (let value = 2; value <= 10; value++) {
+                    this.cards.push(new Card(value.toString() + suits[suitIndex]));
+                }
+                for (let faceCardIndex = 0; faceCardIndex < extra.length; faceCardIndex++) {
+                    this.cards.push(new Card(extra[faceCardIndex] + suits[suitIndex]));
+                }
+            }
         }
-        for (let faceCardIndex = 0; faceCardIndex < extra.length; faceCardIndex++) {
-          this.cards.push(new Card(extra[faceCardIndex] + suits[suitIndex]));
+    }
+
+    shuffle() {
+        for (var i = this.cards.length - 1; i >= 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = temp;
         }
-      }
     }
-  }
 
-  shuffle() {
-    for (var i = this.cards.length - 1; i >= 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = this.cards[i];
-      this.cards[i] = this.cards[j];
-      this.cards[j] = temp;
+    dealRandomCardTo(otherDeck) {
+        this.shuffle();
+        otherDeck.cards.push(this.cards.pop());
     }
-  }
 
-  dealRandomCardTo(otherDeck) {
-    this.shuffle();
-    otherDeck.cards.push(this.cards.pop());
-  }
-
-  addCard(cardToAdd) {
-    this.cards.push(cardToAdd);
-  }
-
-  removeCard(cardToRemove) {
-    for(let i = 0; i < this.cards.length; i++) {
-      if(this.cards[i].card == cardToRemove.card) {
-        return this.cards.splice(i, 1);
-      }
+    addCard(cardToAdd) {
+        this.cards.push(cardToAdd);
     }
-    return "Cannot Remove Card. Card Not Found.";
-  }
 
-  show() {
-    for(let i = 0; i < this.cards.length; i++) {
-      this.cards[i].show();
+    removeCard(cardToRemove) {
+        for (let i = 0; i < this.cards.length; i++) {
+            if (this.cards[i].card == cardToRemove.card) {
+                return this.cards.splice(i, 1);
+            }
+        }
+        return "Cannot Remove Card. Card Not Found.";
     }
-  }
 
-  update(y, amountMove) {
-    for(let i = 0; i < this.cards.length; i++) {
-      this.cards[i].update(y, amountMove);
+    show() {
+        for (let i = 0; i < this.cards.length; i++) {
+            this.cards[i].show();
+        }
     }
-  }
 
-  print() {
-    let toReturn = "[";
-    for(let i = 0; i < this.cards.length - 1; i++) {
-      toReturn += this.cards[i].print() + ", ";
+    update(y, amountMove) {
+        for (let i = 0; i < this.cards.length; i++) {
+            this.cards[i].update(y, amountMove);
+        }
     }
-    return toReturn + this.cards[this.cards.length - 1].print() + "]";
-  }
 
-  printShort() {
-    let toReturn = "[";
-    for(let i = 0; i < this.cards.length - 1; i++) {
-      toReturn += this.cards[i].printShort() + ", ";
+    print() {
+        let toReturn = "[";
+        for (let i = 0; i < this.cards.length - 1; i++) {
+            toReturn += this.cards[i].print() + ", ";
+        }
+        return toReturn + this.cards[this.cards.length - 1].print() + "]";
     }
-    return toReturn + this.cards[this.cards.length - 1].printShort() + "]";
-  }
+
+    printShort() {
+        let toReturn = "[";
+        for (let i = 0; i < this.cards.length - 1; i++) {
+            toReturn += this.cards[i].printShort() + ", ";
+        }
+        return toReturn + this.cards[this.cards.length - 1].printShort() + "]";
+    }
 
 }
